c8c5d607de3131f8fee05d0a094a639fed6317f0
dofi4ka <dofi4ka@yandex.ru>
2026-08-29 02:27:20 +0300

    fix: fix typo

--- lib/presentation/pages/profile/widgets/open_bot_card.dart
+++ lib/presentation/pages/profile/widgets/open_bot_card.dart
@@ -1,4 +1,4 @@
-dimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:url_launcher/url_launcher.dart';
 
